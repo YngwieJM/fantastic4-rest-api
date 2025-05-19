27df273e2f7a749bc3d3d593a9892e251f1bce5b
yngwie <yngwiejmantiri@gmail.com>
2025-05-20 03:14:40 +0800

    Resource, LoginDTO, & IAuth

--- src/main/java/com/juaracoding/fantastic4_rest_api/dto/validation/LoginDTO.java
+++ src/main/java/com/juaracoding/fantastic4_rest_api/dto/validation/LoginDTO.java
@@ -1,5 +1,33 @@
 package com.juaracoding.fantastic4_rest_api.dto.validation;
 
-public class LoginDTO {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
+public class LoginDTO {
+    @NotNull(message = "Username Tidak Boleh Null")
+    @NotBlank(message = "Username Tidak Boleh Blank")
+    @NotEmpty(message = "Username Tidak Boleh Kosong")
+    private String username;
+
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[@_#\\-$])[\\w].{8,15}$",
+            message = "Format Password Tidak Valid")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
