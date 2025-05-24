9dc0609c8574b757fb29ad684527bc7e3f30102a
yngwie <yngwiejmantiri@gmail.com>
2025-05-24 22:51:02 +0800

    Koreksi Pak Paul

--- src/main/java/com/juaracoding/fantastic4_rest_api/core/IAuth.java
+++ src/main/java/com/juaracoding/fantastic4_rest_api/core/IAuth.java
@@ -6,5 +6,5 @@ import org.springframework.http.ResponseEntity;
 
 public interface IAuth<T> {
     public ResponseEntity<Object> login(User user, HttpServletRequest request);//181-190
-    public ResponseEntity<Object> login(Admin admin, HttpServletRequest request);//191-200
+
 }
