4ce17f2636c91bf9175fbd2ee52df33e16451fb7
yngwie <yngwiejmantiri@gmail.com>
2025-05-26 23:28:35 +0800

    Repo

---
+++ src/main/java/com/juaracoding/fantastic4_rest_api/repo/AksesRepo.java
@@ -0,0 +1,7 @@
+package com.juaracoding.fantastic4_rest_api.repo;
+
+import com.juaracoding.fantastic4_rest_api.model.Akses;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AksesRepo extends JpaRepository<Akses, Long> {
+}
