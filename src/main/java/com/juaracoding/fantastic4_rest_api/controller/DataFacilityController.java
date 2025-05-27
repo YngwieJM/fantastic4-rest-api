0cadcacdb64539abeb422fa32ea2a65a1dc828a3
yngwie <yngwiejmantiri@gmail.com>
2025-05-28 00:40:29 +0800

    Menu Contoller DataFacility Prepare

---
+++ src/main/java/com/juaracoding/fantastic4_rest_api/controller/DataFacilityController.java
@@ -0,0 +1,9 @@
+package com.juaracoding.fantastic4_rest_api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("data-facility")
+public class DataFacilityController {
+}
