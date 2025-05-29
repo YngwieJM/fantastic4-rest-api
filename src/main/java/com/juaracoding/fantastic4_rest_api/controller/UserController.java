b6d6c2851c53fcafd2d34b793ba9b8e7c16955b7
yngwie <yngwiejmantiri@gmail.com>
2025-05-29 16:48:20 +0800

    Revert "UserController.java"
    This reverts commit 3f419bf463a8bf6ebfd77de825a93861a67a33ba.

--- src/main/java/com/juaracoding/fantastic4_rest_api/controller/UserController.java
+++ src/main/java/com/juaracoding/fantastic4_rest_api/controller/UserController.java
@@ -1,15 +1,18 @@
 package com.juaracoding.fantastic4_rest_api.controller;
 
 import com.juaracoding.fantastic4_rest_api.dto.validation.ValUserDTO;
+import com.juaracoding.fantastic4_rest_api.model.User;
 import com.juaracoding.fantastic4_rest_api.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.HandlerMapping;
 
 @RestController
 @RequestMapping("user")
@@ -17,25 +20,30 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Qualifier("resourceHandlerMapping")
+    @Autowired
+    private HandlerMapping resourceHandlerMapping;
 
     @PostMapping
     public ResponseEntity<Object> save(@Valid @RequestBody ValUserDTO valUserDTO,
-                                       HttpServletRequest request) {
-        return userService.save(userService.mapToUser(valUserDTO), request);
+                                       HttpServletRequest request){
+        return userService.save(userService.mapToUser(valUserDTO),request);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Object> update(@Valid @RequestBody ValUserDTO valUserDTO,
-                                         @PathVariable String id,
-                                         HttpServletRequest request) {
+                                               @PathVariable String id,
+                                               HttpServletRequest request) {
         return userService.update(id, userService.mapToUser(valUserDTO), request);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> delete(@PathVariable String id,
-                                         HttpServletRequest request) {
+                                          HttpServletRequest request) {
         return userService.delete(id, request);
     }
+
+    /** default page for user */
 
     @GetMapping
     public ResponseEntity<Object> findAll(HttpServletRequest request) {
@@ -49,37 +57,33 @@ public class UserController {
         return userService.findById(id, request);
     }
 
-    @GetMapping("/search/{sort}/{sort-by}/{page}")
+    @GetMapping("/find-by-param/{sort}/{sort-by}/{page}")
     public ResponseEntity<Object> findByParam(@PathVariable String sort,
-                                              @PathVariable("sort-by") String sortBy,
+                                              @PathVariable(value = "sort-by") String sortBy,
                                               @PathVariable Integer page,
                                               @RequestParam Integer size,
                                               @RequestParam String column,
                                               @RequestParam String value,
                                               HttpServletRequest request) {
-        Pageable pageable;
+        Pageable pageable = null;
         sortBy = sortColumn(sortBy);
-        switch (sort.toLowerCase()) {
-            case "desc":
-                pageable = PageRequest.of(page, size, Sort.by(sortBy).descending());
-                break;
-            default:
-                pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
-                break;
+        switch (sort){
+            case"desc":pageable = PageRequest.of(page, size, Sort.by("id").descending()); break;
+            default:pageable = PageRequest.of(page, size, Sort.by("id").ascending()); break;
         }
         return userService.findByParam(pageable, column, value, request);
     }
 
     private String sortColumn(String column) {
-        switch (column) {
-            case "nama":
-            case "email":
-            case "noTelp":
-            case "departemen":
-            case "jabatan":
-                return column;
-            default:
-                return "id";
+        switch (column){
+            case "nama": column = "nama"; break;
+            case "email": column = "email"; break;
+            case "noTelp": column = "noTelp"; break;
+            case "departemen": column = "departemen"; break;
+            case "jabatan": column = "jabatan"; break;
+            default:column= "id"; break;
         }
+        return column;
     }
+
 }
