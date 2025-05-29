65e5af87398db318f1bbed9bbe5039c49539666e
elia <elialisataya@gmail.com>
2025-05-29 11:19:29 +0700

    PesanRepo dan PesanService

---
+++ src/main/java/com/juaracoding/fantastic4_rest_api/repo/PesanRepo.java
@@ -0,0 +1,19 @@
+package com.juaracoding.fantastic4_rest_api.repo;
+
+import com.juaracoding.fantastic4_rest_api.model.Pesan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PesanRepo extends  JpaRepository<Pesan, Long> {
+
+    public Page<Pesan> findByNamaPertemuanContainsIgnoreCase(String nama, Pageable pageable);
+    public Page<Pesan> findByStatusIgnoreCase(String status, Pageable pageable);
+
+    public List<Pesan> findByTanggal(LocalDate tanggal);
+    public List<Pesan> findByUserIDUserID(Long userID);
+    public List<Pesan> findByRuanganIDRuanganID(Long ruanganID);
+}
--- src/main/java/com/juaracoding/fantastic4_rest_api/service/PesanService.java
+++ src/main/java/com/juaracoding/fantastic4_rest_api/service/PesanService.java
@@ -3,8 +3,60 @@ package com.juaracoding.fantastic4_rest_api.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.juaracoding.fantastic4_rest_api.model.Pesan;
+import com.juaracoding.fantastic4_rest_api.repo.PesanRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class PesanService {
+    private final PesanRepo pesanRepo;
+
+    @Autowired
+    public PesanService(PesanRepo pesanRepo) {
+        this.pesanRepo = pesanRepo;
+    }
+
+    public Pesan save(Pesan pesan) {
+        return pesanRepo.save(pesan);
+    }
+
+    public List<Pesan> findAll() {
+        return pesanRepo.findAll();
+    }
+
+    public Optional<Pesan> findById(Long id) {
+        return pesanRepo.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        pesanRepo.deleteById(id);
+    }
+
+    public Page<Pesan> findByNamaPertemuan(String nama, Pageable pageable) {
+        return pesanRepo.findByNamaPertemuanContainsIgnoreCase(nama, pageable);
+    }
+
+    public Page<Pesan> findByStatus(String status, Pageable pageable) {
+        return pesanRepo.findByStatusIgnoreCase(status, pageable);
+    }
+
+    public List<Pesan> findByTanggal(LocalDate tanggal) {
+        return pesanRepo.findByTanggal(tanggal);
+    }
+
+    public List<Pesan> findByUser(Long userId) {
+        return pesanRepo.findByUserIDUserID(userId);
+    }
+
+    public List<Pesan> findByRuangan(Long ruanganId) {
+        return pesanRepo.findByRuanganIDRuanganID(ruanganId);
+    }
 }
