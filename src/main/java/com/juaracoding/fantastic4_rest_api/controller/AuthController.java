a4723c6e6fb143d8ca9b73eb5c268eee2add8969
yngwie <yngwiejmantiri@gmail.com>
2025-05-30 19:28:20 +0800

    Authentication Login

---
+++ src/main/java/com/juaracoding/fantastic4_rest_api/controller/AuthController.java
@@ -0,0 +1,45 @@
+package com.juaracoding.fantastic4_rest_api.controller;
+
+import com.juaracoding.fantastic4_rest_api.dto.validation.LoginDTO;
+import com.juaracoding.fantastic4_rest_api.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("auth")
+public class AuthController {
+    /**
+     * 1. Registrasi
+     * 2. Login
+     * 3. Lupa Password
+     */
+    @Autowired
+    AuthService authService;
+
+//
+//    @PostMapping("/regis")
+//    public ResponseEntity<Object> registration(@Valid @RequestBody RegisDTO regisDTO
+//            , HttpServletRequest request
+//
+//    ){
+//        return authService.regis(authService.mapToUser(regisDTO),request);
+//    }
+//
+//    @PostMapping("/verify-regis")
+//    public ResponseEntity<Object> verifyRegis(@Valid @RequestBody VerifyRegisDTO verifyRegisDTO
+//            , HttpServletRequest request){
+//        return authService.verifyRegis(authService.mapToUser(verifyRegisDTO),request);
+//    }
+
+    @PostMapping("login")
+    public ResponseEntity<Object> login(@Valid @RequestBody LoginDTO loginDTO
+            , HttpServletRequest request){
+        return authService.login(authService.mapToUser(loginDTO),request);
+    }
+}
